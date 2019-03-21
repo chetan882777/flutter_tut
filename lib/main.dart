f7ecea49a3aa676363801b007406201d24e4fb77
chetan882777 <chetan882777@gmail.com>
2019-03-21 00:24:33 -0700

    part 3 implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  Color color = Colors.blue;
   Widget titleSection = Container(
     padding: const EdgeInsets.all(32),
     child: Row(
@@ -46,12 +46,66 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: Text('Flutter layout demo'),
           ),
-          body: Column(
-            children: <Widget>[
-              titleSection
+          body: ListView(
+            children:[
+              Image.asset(
+                'images/lake.jpg',
+                width: 600,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+              titleSection,
+              bottonSelection,
+              textSection
             ],
           ),
         )
     );
   }
+
+
+  Widget bottonSelection = Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _buildButtonColumn(Colors.blue, Icons.call, 'CALL'),
+        _buildButtonColumn(Colors.blue, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(Colors.blue, Icons.share, 'SHARE'),
+      ],
+    ),
+  );
+
+  static Column _buildButtonColumn(Color color, IconData icon, String label){
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon , color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color
+            ),
+          ),
+        )
+
+      ],
+    );
+  }
+
+  Widget textSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Text(
+      'adkfhld alksh lks aslkh aksld dhask hdkl hDHLKS DHkldh LDH ASLDHld sl '
+          'sjdgskjdgaksjdgasjfagsjsdlkKDGLJFGAF SLGLKSD GG KLDGASLD GDAGD LKAG'
+          'SKLDF HDSALK HKLDF KLJA KAJ DHDFAF  LAKHFSD SKSKDS FGSGSDF GSFKAS AK A'
+          ' ALKF GDKFAGSL AAFL FLSFOIQ JDE  F AJF YKMDN DJA JAGM GVZKGAKF GAGL ZL '
+          ' AFGZGH,EI IA  G,Z A EFGAU GMJGZMGWI YHGKWIHKSMDV ZIHAEII FHKIHY LIZL ',
+      softWrap: true,
+    ),
+  );
 }
